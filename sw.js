b12360d8ae345b381c18bf6546eebc5be6239191
Golden Week <parzivalghxz@gmail.com>
2026-06-13 15:44:41 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,14 @@
-const CACHE_NAME = 'golden-week-cache-v1';
+const CACHE_NAME = 'golden-week-cache-v2';
 const assets = [
   './',
   './index.html',
   './style.css',
   './script.js',
   './manifest.json',
-  'https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700;800&family=JetBrains+Mono:wght@500;700&display=swap'
+  './icon.png',
+  'https://fonts.googleapis.com/css2?family=Cinzel:wght@600;700;900&family=Courier+Prime:wght@700&family=JetBrains+Mono:wght@700&display=swap'
 ];
 
-// Instala o Service Worker e guarda os arquivos no cache do celular
 self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
@@ -17,12 +17,10 @@ self.addEventListener('install', e => {
   );
 });
 
-// Ativa o Service Worker
 self.addEventListener('activate', e => {
   e.waitUntil(self.clients.claim());
 });
 
-// Estratégia de rede: Carrega do Cache primeiro para ser ultra rápido
 self.addEventListener('fetch', e => {
   e.respondWith(
     caches.match(e.request).then(cachedResponse => {
